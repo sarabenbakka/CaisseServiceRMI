abc98aedb447c8bf6ec345277bcb258118a1559f
sarabenbakka <benbakkasara@gmail.com>
2025-02-17 13:12:49 +0100

    2nd commit

--- src/CaisseClient.java
+++ src/CaisseClient.java
@@ -8,11 +8,9 @@ public class CaisseClient {
     private String nomServeur;
 
     public CaisseClient(String nomServeur) {
-        this.nomServeur = nomServeur; // Initialisation de la variable nomServeur
+        this.nomServeur = nomServeur;
         try {
-            // Connexion au registre RMI
             Registry registry = LocateRegistry.getRegistry("localhost", 1099);
-            // Recherche du service RMI
             caisse = (CaisseService) registry.lookup("CaisseService");
         } catch (Exception e) {
             System.err.println("Erreur client: " + e.getMessage());
@@ -20,10 +18,8 @@ public class CaisseClient {
         }
     }
 
-    // Méthode pour envoyer une commande
     public void envoyerCommande(Map<String, Integer> plats) {
         try {
-            // Appel de la méthode distante pour ajouter la commande
             caisse.ajouterCommande(nomServeur, plats);
             System.out.println("Commande envoyée avec succès pour " + nomServeur);
         } catch (RemoteException e) {
